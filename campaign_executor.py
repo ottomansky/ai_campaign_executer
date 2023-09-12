23bcfec5700642e22ce31e50d8255d16879eda12
Petr <Petrhunka@users.noreply.github.com>
2023-09-12 21:38:55 +0200

    Update campaign_executor.py

--- campaign_executor.py
+++ campaign_executor.py
@@ -22,8 +22,9 @@ def main():
 
 
     st.title("APP: Twilio campaing approval")
-
-    data = pd.read_csv('/data/in/tables/twilio_sms_campaign_approval_request.csv')
+    file_path = "/data/in/tables/twilio_sms_campaign_approval_request.csv"
+    data = pd.read_csv(file_path)
+    
     # Display the data in an editable table using st.data_editor
     edited_data = st.data_editor(data, num_rows="dynamic", width=1400, height=500)
 
