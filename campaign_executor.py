116ad388c99645f025bca2d95ce87841a6616321
Monika Feigler <monika@feigler.cz>
2023-09-13 09:56:52 +0200

    header, footer

--- campaign_executor.py
+++ campaign_executor.py
@@ -4,7 +4,21 @@ import pandas as pd
 import csv
 import os
 from streamlit_option_menu import option_menu
+import base64
 
+logo_image = os.path.abspath("./app/static/keboola.png")
+
+logo_html = f"""<div style="display: flex; justify-content: flex-end;"><img src="data:image/png;base64,{base64.b64encode(open(logo_image, "rb").read()).decode()}" style="width: 100px; margin-left: -10px;"></div>"""
+html_footer = f"""
+ <div style="display: flex; justify-content: flex-end;margin-top: 12%">
+        <div>
+            <p><strong>Version:</strong> 1.1</p>
+        </div>
+        <div style="margin-left: auto;">
+            <img src="data:image/png;base64,{base64.b64encode(open(logo_image, "rb").read()).decode()}" style="width: 100px;">
+        </div>
+    </div>
+"""
 
 token = st.secrets["kbc_storage_token"]
 url = st.secrets["kbc_url"]
@@ -15,14 +29,15 @@ st.set_page_config(
     
 )
 
-
-
 client_upload = Client(url, token)
 
 def main():
 
-
-    st.title("Twilio Campaing Approval")
+    # Set up Streamlit container with title and logo
+    with st.container():
+        st.markdown(f"{logo_html}", unsafe_allow_html=True)
+        st.title("Twilio Campaing Approval")
+    
     file_path = "/data/in/tables/twilio_sms_campaign_approval_request.csv"
     data = pd.read_csv(file_path)
     
@@ -39,5 +54,10 @@ def main():
         edited_data.to_csv('updated_data.csv.gz', index=False,compression='gzip')
         
         client_upload.tables.load(table_id = 'out.c-twillio-sms-data-preparation.twilio_sms_campaign_approval_request' , file_path='updated_data.csv.gz', is_incremental=False)
+
+    
+    # Display HTML footer
+    st.markdown(html_footer, unsafe_allow_html=True)
+
 if __name__ == '__main__':
     main()
